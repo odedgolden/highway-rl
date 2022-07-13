0c28ab3df6412803f657933c2cba8495029888bb
Oded <Golden>
2022-07-13 20:13:02 +0000

    Working on sac.

---
+++ models/networks.py
@@ -0,0 +1,127 @@
+import os
+import torch as T
+import torch.nn.functional as F
+import torch.nn as nn
+import torch.optim as optim
+from torch.distributions.normal import Normal
+import numpy as np
+
+
+class BaseNetwork(nn.Module):
+    def __init__(self, name, chkpt_dir, lr):
+        super(BaseNetwork, self).__init__()
+        self.name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_path  = os.path.join(self.checkpoint_dir, name + '_sac') 
+        self.lr = lr
+        
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_path)
+        
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_path))
+        
+    def figure_out_device():
+        self.optimizer = optim.Adam(self.parameters(), lr=self.lr)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        
+        self.to(self.device)        
+        
+
+
+        
+class CriticNetwork(BaseNetwork):
+    def __init__(self, lr, input_dims, n_actions, fc1_dims=256, fc2_dims=256, name='critic', chkpt_dir='tmp/sac'):
+        super(CriticNetwork, self).__init__(name=name, chkpt_dir=chkpt_dir, lr=lr)
+        
+        self.input_dims = input_dims
+        self.n_actions = n_actions
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+
+        self.fc1 = nn.Linear(self.input_dims[0] + self.n_actions, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.q = nn.Linear(self.fc2_dims, 1)
+        
+        self.figure_out_device()
+        
+    def forward(self, state, action):
+        action_value = self.fc1(T.cat([state, action], dim=1))
+        action_value = F.relu(action_value)
+        action_value = self.fc2(action_value)
+        action_value = F.relu(action_value)
+        
+        q = self.q(action_value)
+        
+        return q
+
+class ValueNetwork(BaseNetwork):
+    def __init__(self, lr, input_dims, fc1_dims=256, fc2_dims=256, name='value', chkpt_dir='tmp/sac'):
+        super(ValueNetwork, self).__init__(name=name, chkpt_dir=chkpt_dir, lr=lr)
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.v = nn.Linear(self.fc2_dims, 1)
+        
+        self.figure_out_device()
+        
+    def forward(self, state):
+        state_value = self.fc1(state)
+        state_value = F.relu(state_value)
+        state_value = self.fc2(state)
+        state_value = F.relu(state_value)
+        
+        v = self.v(action_value)
+        
+        return v
+    
+
+class ActorNetwork(BaseNetwork):
+    def __init__(self, lr, input_dims, max_action, fc1_dims=256, fc2_dims=256, n_actions=2, name='actor', chkpt_dir='tmp/sac'):
+        super(ActorNetwork, self).__init__(name=name, chkpt_dir=chkpt_dir, lr=lr)
+        self.input_dims = input_dims
+        self.n_actions = n_actions
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.max_action = max_action
+        self.reparam_noise = 1e-6
+        
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.mu = nn.Linear(self.fc2_dims, self.n_actions)
+        self.sigma = nn.Linear(self.fc2_dims, self.n_actions)
+
+        self.figure_out_device()
+
+
+    def forward(self, state):
+        prob = self.fc1(state)
+        prob = F.relu(prob)
+        prob = self.fc2(prob)
+        prob = F.relu(prob)
+        
+        mu = self.mu(prob)
+        sigma = self.sigma(prob)
+        
+        sigma = T.clamp(sigma, min=self.reparam_noise, max=1)
+        
+        return mu, sigma
+    
+    def sample_normal(self, state, reparameterize=True):
+        mu, sigma = self.forward(state)
+        probaabilities = Normal(mu, sigma)
+        
+        if reparameterize:
+            actions = probaabilities.rsample()
+        else:
+            actions = probaabilities.sample()
+            
+        action = T.tanh(actions)*T.tensor(self.max_action).to(self.device)
+        log_probs = probaabilities.log_prob(actions)
+        log_probs -= T.log(1-action.pow(2) + self.reparam_noise)
+        log_probs = log_probs.sum(1, keepdim=True)
+        
+        return action, log_probs
---
+++ models/utils.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_learning_curve(x, scores, figure_file):
+    running_avg = np.zeros(len(scores))
+    for i in range(len(running_avg)):
+        running_avg[i] = np.mean(scores[max(0, i-100):(i+1)])
+    plt.plot(x, running_avg)
+    plt.title('Running average of previous 100 scores')
+    plt.savefig(figure_file)
+    
+
+class ReplayBuffer():
+    def __init__(self, max_size, input_shape, n_actions):
+        self.memory_size = max_size
+        self.memory_counter = 0
+        self.state_memory = np.zeros((self.memory_size, *input_shape))
+        self.new_state_memory = np.zeros((self.memory_size, *input_shape))
+        self.action_memory = np.zeros((self.memory_size, n_actions))
+        self.reward_memory = np.zeros(self.memory_size)
+        self.terminal_memory = np.zeros(self.memory_size, dtype=np.bool)
+    
+    def store_transition(self, state, action, reward, new_state, done):
+        
+        index = self.memory_counter % self.memory_size
+        
+        self.state_memory[index] = state
+        self.new_state_memory[index] = new_state
+        self.action_memory[index] = action
+        self.reward_memory[index] = reward
+        self.terminal_memory[index] = done
+        
+        self.memory_counter += 1
+        
+    def sample_buffer(self, batch_size):
+        max_memory = min(self.memory_counter, self.memory_size)
+        
+        batch = np.random.choice(max_memory, batch_size)
+        states = self.state_memory[batch]
+        new_states = self.new_state_memory[batch]
+        actions = self.action_memory[batch]
+        rewards = self.reward_memory[batch]
+        dones = self.terminal_memory[batch]
+
+        return states, actions, rewards, new_states, dones 
